5e5b6c12605af79fc481b6eb925d1ea4f4d5bba7
mainieri@tm <mainieri.tm@programmer.net>
2021-03-17 09:41:55 +0100

    Commit ticket #1 e #2

---
+++ kezapp-gruppo3-server/src/main/java/it/iad/kezappgruppo3/KezappGruppo3Application.java
@@ -0,0 +1,13 @@
+package it.iad.kezappgruppo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KezappGruppo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KezappGruppo3Application.class, args);
+	}
+
+}
